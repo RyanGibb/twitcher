759d9d7cfeda51ec5c04a4390fa5c22d72526343
ryangibb <ryangibb321@gmail.com>
2019-03-02 16:50:23 +0000

    frontend websockets

---
+++ static/script.js
@@ -0,0 +1,33 @@
+
+"use strict";
+
+//----------------------------------------------------------------------------
+//                              WebSocket Client
+//----------------------------------------------------------------------------
+
+let wsUrl = "wss://" + location.hostname + ":" + location.port + "/gym-occupancy/";
+let ws = new WebSocket(wsUrl);
+
+ws.onopen = function() {
+  requestLast24Hrs();
+}
+
+ws.onclose = function() {
+  alert("WebSocket closed. Please reload the page.");
+}
+
+ws.onerrer = function(e) {
+  alert("WebSocket Error: " + e + ". Please reload the page.");
+}
+
+ws.onmessage = function(m) {
+  let messageString = m.data;
+  //console.log("<- rx " + messageString);
+  let message = JSON.parse(messageString);
+  handleMessage(message);
+}
+
+function sendMessage(messageString) {
+  //console.log("-> tx " + messageString);
+  ws.send(messageString);
+}
