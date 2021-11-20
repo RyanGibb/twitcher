e8674c8e6ac7b5325b44b780f1350f16dc9a75eb
Ryan Gibb <ryangibb321@gmail.com>
2021-11-20 10:42:24 +0000

    test

--- functions/userinfo.js
+++ functions/userinfo.js
@@ -1,5 +1,24 @@
 
 const Twitter = require('twitter')
+  
+export const onRequestPost = async ({ request }) => {
+    const { name } = await request.json()
+//   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
+//   let handle = req.body.handle
+//   if (!handle) {
+//     console.log('Missing handle')
+//     res.status(500).json({ error: 'Missing handle' })
+//     return
+//   }
+//   try {
+//     let user = await getUser(handle)
+//     res.json(user)
+//   } catch(error) {
+//     console.log(error)
+//     res.status(500).json({ error: 'Error getting user info' })
+//   }
+    return new Response(`Hello, ${name}!`)
+}
 
 export const onRequestGet = () => {
 //   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
@@ -16,5 +35,5 @@ export const onRequestGet = () => {
 //     console.log(error)
 //     res.status(500).json({ error: 'Error getting user info' })
 //   }
-    return new Response("hello")
+    return new Response(`Hello`)
 }
