563d9dfa6de557fff275522ac5e5713a84a797ee
Ryan Gibb <ryangibb321@gmail.com>
2022-01-19 12:38:23 +0000

    cf workers spike

---
+++ functions/index.js
@@ -0,0 +1,168 @@
+
+// require("dotenv").config()
+
+// // Twitter API
+
+// const Twitter = require('twitter')
+// const tcom = require('thesaurus-com')
+// const WordPOS = require('wordpos')
+// const wordpos = new WordPOS()
+
+// const client = new Twitter({
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token_key: process.env.ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET
+// })
+
+// const tweet_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?exclude_replies=true&include_rts=false&screen_name='
+
+// const user_url = 'https://api.twitter.com/1.1/users/lookup.json?screen_name='
+
+// clientGetPromise = (...args) => {
+//   return new Promise( (resolve, reject) =>
+//     client.get(...args,
+//       (error, data, response) => {
+//         if (error) reject(error)
+//         resolve( [data, response] )
+//       }
+//     )
+//   )
+// }
+
+// wordGetPOSPromise = (...args) => {
+//   return new Promise(resolve =>
+//     wordpos.getPOS(...args, obj => resolve(obj))
+//   )
+// }
+
+// async function getUser(handle) {
+//   try {
+//     let [user_info, _] = await clientGetPromise(user_url + handle)
+//     let user = {
+//       'follower_count':    user_info[0].followers_count,
+//       'tweet_count':       user_info[0].statuses_count,
+//       'profile_image_url': user_info[0].profile_image_url
+//     }
+//     return user
+//   } catch(error) {
+//     console.log(error)
+//     throw 'User doesn\'t exist'
+//   }
+// }
+
+// async function getRecentTweets(handle){
+//   let [tweets, _] = await clientGetPromise(tweet_url + handle)
+//   var recent_tweets = []
+//   for (let i = 0; i < tweets.length; i++) {
+//     let tweet = tweets[i]
+//     let body = tweet.text
+//     let timestamp = (new Date(tweet.created_at)).toLocaleDateString()
+//     recent_tweets.push({
+//       body,
+//       timestamp
+//     })
+//   }
+//   return recent_tweets
+// }
+
+// async function getBlankedTweets(handle){
+//   let [tweets, _] = await clientGetPromise(tweet_url + handle)
+//   let recent_tweets = []
+//   for (let i = 0; i < tweets.length; i+=1) {
+//     let tweet = tweets[i]
+//     let body = tweet.text
+//     let obj = await wordGetPOSPromise(body)
+//     let wordArray = obj.nouns.concat(obj.verbs, obj.adjectives, obj.adverbs)
+//     if (wordArray.length > 0) {
+//       var word = wordArray[Math.floor(Math.random()*wordArray.length)]
+//       let possibilities = tcom.search(word)
+//       let body = tweet.text
+//       let timestamp = (new Date(tweet.created_at)).toLocaleDateString()
+//       recent_tweets.push({body, timestamp, word, possibilities})
+//     }
+//   }
+//   return recent_tweets
+// }
+
+// // HTTP Server
+
+// const express = require('express')
+// const port = process.env.PORT || 8080
+// const app = express()
+// const static_dir = 'static'
+
+// app.use(express.json())
+// app.use(express.static(static_dir))
+
+// app.post('/userinfo', async (req, res) => {
+//   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
+//   let handle = req.body.handle
+//   if (!handle) {
+//     console.log('Missing handle')
+//     res.status(500).json({ error: 'Missing handle' })
+//     return
+//   }
+//   try {
+//     let user = await getUser(handle)
+//     res.json(user)
+//   } catch(error) {
+//     console.log(error)
+//     res.status(500).json({ error: 'Error getting user info' })
+//   }
+// })
+
+// app.post('/recentTweets', async (req, res) => {
+//   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
+//   let handle = req.body.handle
+//   if (!handle) {
+//     console.log('Missing handle')
+//     res.status(500).json({ error: 'Missing handle' })
+//     return
+//   }
+//   try {
+//     let recent_tweets = await getRecentTweets(handle)
+//     res.json(recent_tweets)
+//   } catch(error) {
+//     console.log(error)
+//     res.status(500).json({ error: 'Error getting recent tweets' })
+//   }
+// })
+
+// app.post('/recentTweetsBlanked', async (req, res) => {
+//   transmissionLog('blank -> rx ' + JSON.stringify(req.body))
+//   let handle = req.body.handle
+//   if(!handle) {
+//     console.log('Missing handle')
+//     res.status(500).json({ error: 'Missing handle' })
+//     return
+//   }
+//   try {
+//     let recent_tweets = await getBlankedTweets(handle)
+//     res.json(recent_tweets)
+//   } catch(error) {
+//     console.log(error)
+//     res.status(500).json({ error: 'Error getting blanked recent tweets' })
+//   }
+// })
+
+// app.listen(port, () => {
+//   console.log('Listening for HTTP requests on port ' + port)
+// })
+
+// const maxTransmitionLogLength = 200
+
+// function transmissionLog(message){
+//   console.log((message.length > maxTransmitionLogLength ? message.slice(0, maxTransmitionLogLength) + '...' : message))
+// }
+
+// GET requests to /filename would return "Hello, world!"
+export const onRequestGet = () => {
+  return new Response("Hello, world!")
+}
+
+// POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
+export const onRequestPost = async ({ request }) => {
+  const { name } = await request.json()
+  return new Response(`Hello, ${name}!`)
+}
--- functions/userinfo.js
+++ functions/userinfo.js
@@ -1,39 +1,65 @@
 
-// const Twitter = require('twitter')
-  
+const Twitter = require('twitter')
+
+const client = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+clientGetPromise = (...args) => {
+	return new Promise( (resolve, reject) =>
+	client.get(...args,
+		(error, data, response) => {
+		if (error) reject(error)
+		resolve( [data, response] )
+		}
+	)
+	)
+}
+
+async function getUser(handle) {
+	try {
+	let [user_info, _] = await clientGetPromise(user_url + handle)
+	let user = {
+		'follower_count':    user_info[0].followers_count,
+		'tweet_count':       user_info[0].statuses_count,
+		'profile_image_url': user_info[0].profile_image_url
+	}
+	return user
+	} catch(error) {
+	console.log(error)
+	throw 'User doesn\'t exist'
+	}
+}
+	
 export const onRequestPost = async ({ request }) => {
-    const { name } = await request.json()
-//   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
-//   let handle = req.body.handle
-//   if (!handle) {
-//     console.log('Missing handle')
-//     res.status(500).json({ error: 'Missing handle' })
-//     return
-//   }
-//   try {
-//     let user = await getUser(handle)
-//     res.json(user)
-//   } catch(error) {
-//     console.log(error)
-//     res.status(500).json({ error: 'Error getting user info' })
-//   }
-    return new Response(`Hello, ${name}!`)
+	const { handle } = await request.json()
+	if (!handle) {
+		return new Response({ error: 'Missing handle' }, { status: 500 })
+	}
+	try {
+		let user = await getUser(handle)
+		return new Response(user)
+	} catch (error) {
+		return new Response({ error: 'Error getting user info' }, { status: 500 })
+	}
 }
 
 export const onRequestGet = () => {
-//   transmissionLog('userinfo -> rx ' + JSON.stringify(req.body))
-//   let handle = req.body.handle
-//   if (!handle) {
-//     console.log('Missing handle')
-//     res.status(500).json({ error: 'Missing handle' })
-//     return
-//   }
-//   try {
-//     let user = await getUser(handle)
-//     res.json(user)
-//   } catch(error) {
-//     console.log(error)
-//     res.status(500).json({ error: 'Error getting user info' })
-//   }
-    return new Response(`Hello`)
+//	 let handle = req.body.handle
+//	 if (!handle) {
+//		 console.log('Missing handle')
+//		 res.status(500).json({ error: 'Missing handle' })
+//		 return
+//	 }
+//	 try {
+//		 let user = await getUser(handle)
+//		 res.json(user)
+//	 } catch(error) {
+//		 console.log(error)
+//		 res.status(500).json({ error: 'Error getting user info' })
+//	 }
+	return new Response(`Hello`)
 }
