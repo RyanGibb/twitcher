567b7a406e7993d104cb0570c2e98e186c8938dd
Ryan Gibb <ryangibb321@gmail.com>
2022-11-30 15:04:36 +0000

    update

--- module.nix
+++ module.nix
@@ -55,10 +55,6 @@ let cfg = config.services.twitcher; in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       environment.PORT = "${builtins.toString cfg.port}";
-      preStart = ''
-        
-        
-      '';
       serviceConfig = {
         ExecStart = "${pkgs.nodejs}/bin/node .";
         EnvironmentFile = cfg.dotenvFile;
@@ -75,48 +71,48 @@ let cfg = config.services.twitcher; in
         RuntimeDirectory = "twitcher";
         RuntimeDirectoryMode = "0750";
         # Cache directory and mode
-        CacheDirectory = "nginx";
+        CacheDirectory = "twitcher";
         CacheDirectoryMode = "0750";
         # Logs directory and mode
-        LogsDirectory = "nginx";
+        LogsDirectory = "twitcher";
         LogsDirectoryMode = "0750";
         # Proc filesystem
-        ProcSubset = "pid";
-        ProtectProc = "invisible";
-        # New file permissions
-        UMask = "0027"; # 0640 / 0750
-        # Capabilities
-        AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" "CAP_SYS_RESOURCE" ];
-        CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" "CAP_SYS_RESOURCE" ];
-         # Security
-        NoNewPrivileges = true;
-        # Sandboxing (sorted by occurrence in https://www.freedesktop.org/software/systemd/man/systemd.exec.html)
-        ProtectSystem = "strict";
-        ProtectHome = true;
-        PrivateTmp = true;
-        PrivateDevices = true;
-        ProtectHostname = true;
-        ProtectClock = true;
-        ProtectKernelTunables = true;
-        ProtectKernelModules = true;
-        ProtectKernelLogs = true;
-        ProtectControlGroups = true;
-        RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
-        RestrictNamespaces = true;
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        RemoveIPC = true;
-        PrivateMounts = true;
-        # System Call Filtering
-        SystemCallArchitectures = "native";
-        SystemCallFilter = [ "~@cpu-emulation @debug @keyring @mount @obsolete @privileged @setuid" ];
+        #ProcSubset = "pid";
+        #ProtectProc = "invisible";
+        ## New file permissions
+        #UMask = "0027"; # 0640 / 0750
+        ## Capabilities
+        #AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" "CAP_SYS_RESOURCE" ];
+        #CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" "CAP_SYS_RESOURCE" ];
+        # # Security
+        #NoNewPrivileges = true;
+        ## Sandboxing (sorted by occurrence in https://www.freedesktop.org/software/systemd/man/systemd.exec.html)
+        #ProtectSystem = "strict";
+        #ProtectHome = true;
+        #PrivateTmp = true;
+        #PrivateDevices = true;
+        #ProtectHostname = true;
+        #ProtectClock = true;
+        #ProtectKernelTunables = true;
+        #ProtectKernelModules = true;
+        #ProtectKernelLogs = true;
+        #ProtectControlGroups = true;
+        #RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+        #RestrictNamespaces = true;
+        #LockPersonality = true;
+        #MemoryDenyWriteExecute = true;
+        #RestrictRealtime = true;
+        #RestrictSUIDSGID = true;
+        #RemoveIPC = true;
+        #PrivateMounts = true;
+        ## System Call Filtering
+        #SystemCallArchitectures = "native";
+        #SystemCallFilter = [ "~@cpu-emulation @debug @keyring @mount @obsolete @privileged @setuid" ];
       };
     };
 
     # requires dns module
-    custom.hosting.dns.records = [
+    dns.records = [
       {
         name = "${cfg.domain}.";
         type = "CNAME";
